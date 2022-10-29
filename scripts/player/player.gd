2df690360fae5edc8f856890b658a1f952ed3ef2
Gui-dev <adrianon10@gmail.com>
2022-10-29 03:13:22 -0300

    feat: add helper function _animations

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,6 +4,10 @@ class_name Player
 
 var speed: float = 200.0
 var motion_velocity: Vector2 = Vector2.ZERO
+var weapon: String = 'pistol'
+onready var animation_legs: AnimationTree = $sprites/legs/AnimationTree
+onready var animation_body: AnimationTree = $sprites/body/AnimationTree
+onready var animation_node: AnimationNodeStateMachinePlayback = animation_body.get('parameters/playback')
 
 
 func _process(delta: float) -> void:
@@ -13,7 +17,13 @@ func _process(delta: float) -> void:
   else:
     motion_velocity = motion_velocity.move_toward(Vector2.ZERO, speed)
   look_at(get_global_mouse_position())
+  _animations()
     
 
 func _physics_process(delta: float) -> void:
   motion_velocity = move_and_slide(motion_velocity)
+
+
+func _animations() -> void:
+  animation_legs.set('parameters/blend_position', motion_velocity)
+  animation_body.set('parameters/%s/blend_position' % weapon, motion_velocity.angle())
