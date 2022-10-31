f0c3cfc72c29693f2795b23099f587bbcda2160a
Gui-dev <adrianon10@gmail.com>
2022-10-31 03:30:52 -0300

    feat: add helper function _shoot

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,12 +2,16 @@ extends KinematicBody2D
 class_name Player
 
 
+const Bullet = preload('res://scenes/prefabs/bullet.tscn')
 var speed: float = 200.0
 var motion_velocity: Vector2 = Vector2.ZERO
 var weapon: String = 'pistol'
+var weapon_timer: float = 0.0
+var cooldown: float = 0.3
 onready var animation_legs: AnimationTree = $sprites/legs/AnimationTree
 onready var animation_body: AnimationTree = $sprites/body/AnimationTree
 onready var animation_node: AnimationNodeStateMachinePlayback = animation_body.get('parameters/playback')
+onready var barrel: Position2D = $barrel
 
 
 func _process(delta: float) -> void:
@@ -18,6 +22,11 @@ func _process(delta: float) -> void:
     motion_velocity = motion_velocity.move_toward(Vector2.ZERO, speed)
   look_at(get_global_mouse_position())
   _animations()
+  
+  weapon_timer -= delta
+  if Input.is_action_pressed('ui_shoot') and weapon_timer <= 0:
+    weapon_timer = cooldown 
+    _shoot()
     
 
 func _physics_process(delta: float) -> void:
@@ -27,3 +36,11 @@ func _physics_process(delta: float) -> void:
 func _animations() -> void:
   animation_legs.set('parameters/blend_position', motion_velocity)
   animation_body.set('parameters/%s/blend_position' % weapon, motion_velocity.angle())
+
+
+func _shoot() -> void:
+  var bullet = Bullet.instance()
+  bullet.global_position = barrel.global_position
+  bullet.rotation_degrees = rotation_degrees
+  bullet.apply_impulse(Vector2.ZERO, Vector2(1000, 0).rotated(rotation))
+  get_tree().root.call_deferred('add_child', bullet)
