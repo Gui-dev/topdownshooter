7417af499c681228c4a7e6162e48e52705a24137
Gui-dev <adrianon10@gmail.com>
2022-11-09 03:25:57 -0300

    feat: add houses interactions

---
+++ scripts/interactions/house.gd
@@ -0,0 +1,33 @@
+extends StaticBody2D
+class_name House
+
+onready var texture_house_out: Sprite = $texture_house_out
+onready var tween: Tween = $tween
+
+
+func _on_area_house_body_entered(body: Node) -> void:
+  if body.name.match('Player'):
+    tween.interpolate_property(
+      texture_house_out, 
+      'modulate:a',
+      1,
+      0,
+      2,
+      Tween.TRANS_LINEAR,
+      Tween.EASE_OUT  
+    )
+    tween.start()
+
+
+func _on_area_house_body_exited(body: Node) -> void:
+  if body.name.match('Player'):
+    tween.interpolate_property(
+      texture_house_out, 
+      'modulate:a',
+      0,
+      1,
+      0.3,
+      Tween.TRANS_LINEAR,
+      Tween.EASE_OUT  
+    )
+    tween.start()
